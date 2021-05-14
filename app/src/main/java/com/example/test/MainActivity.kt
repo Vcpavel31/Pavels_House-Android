b510209918ad047ef57c19146948220b5ca52518
Vcpavel31 <37764821+Vcpavel31@users.noreply.github.com>
2021-05-14 10:18:38 +0200

    Some Progress
    Added TODO
    Created some svg

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test.databinding.ActivityMainBinding
+
+// TODO:
+// Remove top panel
+// How to make multiple screens ???
+// Getting all info from mysql... Depending on connection - Probably some cache to file
+// Some way of backuper...
+// As example old web interface
+// 1) Right top corner connection signalization (Local Wifi; VPN; Unsecure Connection; No connection)
+// 2) User settings - Primary currency; Menu organisation; Add this device...; Link this device...; Sign-out
+// 3) Admin settings - Editing roles
+// 4) Films - Seen films; Adding new films; serials
+// 5) Devices - Network map; Adding devices; Removing devices; Connections ???; IP list; Ping to devices
+// 6) Mapy.cz - Integrate with custom points from DB... Maybe some grouping
+// 7) Task manager - showing HDO; Printer task; ...
+// 8) Password manager - Probably use OpenSource -> KeePass it has clients for Linux; WIN; PHP; Android
+
+// 1) WIFI SSID: - ICON > WIFI - wifi_.svg / VPN - vpn_.svg / Unsecure -
+// https://stackoverflow.com/questions/21391395/get-ssid-when-wifi-is-connected
+//
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.MenuIcon.setOnClickListener {
+            //Slide of menu from left side
+            //black blur to other parts of screen
+           binding.HouseName.setText("")
+        }
     }
 }
