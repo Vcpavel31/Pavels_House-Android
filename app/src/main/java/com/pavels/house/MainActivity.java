f1245cf528a1859a7d4f159143d94a57847dff45
Vcpavel31 <37764821+Vcpavel31@users.noreply.github.com>
2021-05-19 16:06:09 +0200

    Popup
    Popup is now showing current network

--- app/src/main/java/com/pavels/house/MainActivity.java
+++ app/src/main/java/com/pavels/house/MainActivity.java
@@ -7,21 +7,14 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
-import android.net.wifi.WifiInfo;
-import android.content.Intent;
 import android.widget.TextView;
-
-import java.lang.Math;
-import java.net.IDN;
-import java.net.NetworkInterface;
 
 // TODO:
 // Remove top panel
@@ -47,6 +40,18 @@ import java.net.NetworkInterface;
 public class MainActivity extends AppCompatActivity {
 
     Dialog Network_popup;
+    private Handler handler = new Handler();
+    private int network_state = 0;
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // The method you want to call every now and then.
+            Network(findViewById(android.R.id.content).getRootView());
+            Log.d("Handler", "Running");
+            handler.postDelayed(this,30000); // 2000 = 2 seconds. This time is in millis.
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +59,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Network_popup = new Dialog(this);
+        Network(findViewById(android.R.id.content).getRootView());
+        handler.postDelayed(runnable, 30000);
 
     }
 
-    public void ShowMenu(View view)
-    {
+    public void ShowMenu(View view) {
 
     }
 
@@ -107,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         return cm.getNetworkInfo(ConnectivityManager.TYPE_VPN).isConnectedOrConnecting();
     }
 
+    // TODO
     private boolean checkWIFI() {
         if (isNetworkConnected() == 2){
             // Get connection's names if they are matching to home, test ping to space
@@ -115,8 +122,11 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    public void Test_Button(View view)
-    {
+    public void Test_Button(View view){
+
+    }
+
+    public void Network(View view) {
 
         ImageView no_connection = (ImageView) findViewById(R.id.No_Connection);
         ImageView unsecure = (ImageView) findViewById(R.id.Unsecure);
@@ -125,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView mobile = (ImageView) findViewById(R.id.Mobile);
 
         if (checkVPN() && SpaceIsConnected()) {
+            this.network_state = 2;
             vpn.setVisibility(View.VISIBLE);
             unsecure.setVisibility(View.GONE);
             wifi.setVisibility(View.GONE);
@@ -132,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             mobile.setVisibility(View.GONE);
         }
         else if (checkWIFI() && SpaceIsConnected()) {
+            this.network_state = 1;
             wifi.setVisibility(View.VISIBLE);
             vpn.setVisibility(View.GONE);
             unsecure.setVisibility(View.GONE);
@@ -139,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
             mobile.setVisibility(View.GONE);
         }
         else if (isNetworkConnected() == 1) {
+            this.network_state = 3;
             wifi.setVisibility(View.GONE);
             vpn.setVisibility(View.GONE);
             unsecure.setVisibility(View.GONE);
@@ -146,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
             mobile.setVisibility(View.VISIBLE);
         }
         else if (O2IsConnected()) {
+            this.network_state = 4;
             wifi.setVisibility(View.GONE);
             vpn.setVisibility(View.GONE);
             unsecure.setVisibility(View.VISIBLE);
@@ -153,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
             mobile.setVisibility(View.GONE);
         }
         else{
+            this.network_state = 5;
             wifi.setVisibility(View.GONE);
             vpn.setVisibility(View.GONE);
             unsecure.setVisibility(View.GONE);
@@ -162,22 +177,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void ShowNetwork(View view)
-    {
+    public void ShowNetwork(View view) {
+        Log.d("Popup", "Show Network");
+        Network(findViewById(android.R.id.content).getRootView());
         TextView txtclose;
-        Button btnFollow;
+        TextView ConnectionName;
+        ImageView ConnectionIcon;
         Network_popup.setContentView(R.layout.networksatuspopup);
         txtclose =(TextView) Network_popup.findViewById(R.id.txtclose);
-        txtclose.setText("M");
-        //btnFollow = (Button) Network_popup.findViewById(R.id.btnfollow);
+        ConnectionName =(TextView) Network_popup.findViewById(R.id.ConnectionName);
+        ConnectionIcon =(ImageView) Network_popup.findViewById(R.id.ConnectionIcon);
+        switch(network_state) {
+            case 1:
+                ConnectionName.setText("WIFI connection");
+                ConnectionIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_wifi_24));
+                break;
+            case 2:
+                ConnectionName.setText("VPN connection");
+                ConnectionIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_vpn_lock_24));
+                break;
+            case 3:
+                ConnectionName.setText("Mobile data");
+                ConnectionIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_import_export_24));
+                break;
+            case 4:
+                ConnectionName.setText("Unsecure");
+                ConnectionIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_cloud_24));
+                break;
+            case 5:
+                ConnectionName.setText("No connection");
+                ConnectionIcon.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_close_24));
+                break;
+        }
+
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Popup", "Close Network");
                 Network_popup.dismiss();
             }
         });
+
         Network_popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         Network_popup.show();
     }
+
 }
 
